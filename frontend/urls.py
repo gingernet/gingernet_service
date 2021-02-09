0647b7972524fbf4a6ae0f9dd8b1bd7084b401d4
shijiang <guoshijiang2012@163.com>
2021-02-09 22:14:42 +0800

    online msg update

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from django.urls import path, include
 from frontend.views import (
     index, product, solution, research, comp_dyn, partner,
-    about, comp_dyn_detail
+    about, comp_dyn_detail, online_msg
 )
 
 urlpatterns = [
@@ -11,6 +11,7 @@ urlpatterns = [
     path(r'comp_dyn', comp_dyn, name='comp_dyn'),
     path(r'partner', partner, name='partner'),
     path(r'about', about, name='about'),
+    path(r'online_msg', online_msg, name='online_msg'),
     path(r'product-<int:id>.html', product, name='product'),
     path(r'solution-<int:id>.html', solution, name='solution'),
     path(r'comp_dyn_detail-<int:id>.html', comp_dyn_detail, name='comp_dyn_detail'),
